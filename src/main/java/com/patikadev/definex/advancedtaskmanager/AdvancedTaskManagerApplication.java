d12b354e66878cbadce491f59030c81e0e4432b4
cagri-kilic <cagrikilic96@gmail.com>
2025-03-23 19:38:01 +0300

    Application class was added.

---
+++ src/main/java/com/patikadev/definex/advancedtaskmanager/AdvancedTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.patikadev.definex.advancedtaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedTaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedTaskManagerApplication.class, args);
+    }
+
+}
