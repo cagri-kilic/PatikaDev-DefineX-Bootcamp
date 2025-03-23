051e5046f851984d5f563501254d48b7d6cdf35b
cagri-kilic <cagrikilic96@gmail.com>
2025-03-23 19:38:10 +0300

    ApplicationTest class was added.

---
+++ src/test/java/com/patikadev/definex/advancedtaskmanager/AdvancedTaskManagerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.patikadev.definex.advancedtaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AdvancedTaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
